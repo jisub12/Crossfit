bbc7ebe530b8145a99634fb4ac0fe2477eff216f
jisub12 <jisub11@nate.com>
2023-06-26 19:54:05 +0900

    첫 커밋

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+app.use(cors({
+    origin : "http://127.0.0.1:5502/",
+    credentials : true
+}));
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    const page = fs.readFileSync('./public/crossfit.html', "utf-8");
+    res.send(page);
+})
+
+app.listen(7070, () => {
+    console.log("7070 Server Open");
+});
